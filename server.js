4e04b20954a09f92f39fb834b09af593894f6a52
Salem Ezz <sara2in@Salems-MacBook-Pro.local>
2022-03-04 10:32:50 -0500

    finished

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const http = require("http");
-
+const PORT = 8080;
+const HOST = "127.0.0.1";
 // YOUR NODE SERVER CODE GOES HERE
+const server = http.createServer((request, response) => {
+    response.end("Hello, Galvanize!");
+  });
+
+  server.listen(PORT, HOST, () => {
+    console.log(`Server running at ${HOST}:${PORT}/`);
+  });
